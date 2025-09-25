7ee1f9b5897e762a1a568a48b9002ec8dbd34170
nandhinikrishnamoorthy111 <nandhinikrishnamoorthy111@gmail.com>
2025-09-25 08:20:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let num="8";
+num=Number(num);
+if(num %2==0){
+    console.log("The given number is even");
+
+}
+else
+    {
+    console.log("The given numer is odd");
+}
+let day = 7;
+switch(day){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:    
+    console.log("it's a weekday");
+    break;
+    case 6:
+    case 7: 
+    console.log("it's a weekend");
+    break;
+}
